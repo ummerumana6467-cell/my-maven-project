d01291c0b828ec4c1bb5a50ee6a0bd9ed341f30e
ISE <ISE@ITLAB10SY06>
2026-03-23 10:36:02 +0530

    Initial commit

---
+++ eclipse-workspace/maven-archetype-quickstart/src/main/java/org/apache/maven/archetype/maven_archetype_quickstart/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.archetype.maven_archetype_quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
